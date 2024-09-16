77a39dc46fa346abd67b1f29afa582c65de219fa
Iris Veress <irisveress@gmail.com>
2024-09-16 21:24:36 +0300

    added movies/shows pages

--- src/pages/MoviesPage.js
+++ src/pages/MoviesPage.js
@@ -1,7 +1,13 @@
+import VideoGrid from "../components/VideoGrid";
+
 export default function MoviesPage() {
+  const videos = JSON.parse(localStorage.getItem("videos"));
+  const movies = videos.filter((video) => video.type === "movie");
+
   return (
     <div className="py-4">
       <h1 className="text-4xl text-white font-bold mb-2">Movies:</h1>
+      <VideoGrid videos={movies} />
     </div>
   );
 }
--- src/pages/ShowsPage.js
+++ src/pages/ShowsPage.js
@@ -1,7 +1,13 @@
+import VideoGrid from "../components/VideoGrid";
+
 export default function ShowsPage() {
+  const videos = JSON.parse(localStorage.getItem("videos"));
+  const shows = videos.filter((video) => video.type === "show");
+
   return (
     <div className="py-4">
       <h1 className="text-4xl text-white font-bold mb-2">Shows:</h1>
+      <VideoGrid videos={shows} />
     </div>
   );
 }
