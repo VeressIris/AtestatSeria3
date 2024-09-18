48b85392297f01bbb20229e8c8fbb0c18d1a74bf
Iris Veress <irisveress@gmail.com>
2024-09-18 20:56:23 +0300

    added video start after 2 clicks + ads

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -19,15 +19,21 @@ export default function VideoPlayer({ thumbnail, videoSrc }) {
 
   const menuPlayButton = useRef();
 
-  function play() {
-    video.current.play();
-    playButton.current.style.display = "none";
-    setMaxTime(convertTime(video.current.duration));
+  const [clicks, setClicks] = useState(0);
+  const ads = ["https://fireship.io", "https://x.com/fireship_dev"];
+  function showAds() {
+    if (clicks >= 2) return;
+    const ad = ads[Math.floor(Math.random() * ads.length)];
+    window.open(ad, "_blank");
   }
 
   const [isPlaying, setIsPlaying] = useState(false);
   const [started, setStarted] = useState(false);
   function menuPlay() {
+    showAds();
+    setClicks(clicks + 1);
+    if (clicks < 2) return;
+
     setStarted(true);
     if (video.current.paused) {
       setIsPlaying(true);
@@ -141,7 +147,6 @@ export default function VideoPlayer({ thumbnail, videoSrc }) {
               viewBox="0 -960 960 960"
               fill="#ffffff"
               ref={playButton}
-              onClick={play}
             >
               <path d="m380-300 280-180-280-180v360ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
             </svg>
@@ -153,7 +158,6 @@ export default function VideoPlayer({ thumbnail, videoSrc }) {
               width="56px"
               fill="#ffffff"
               ref={playButton}
-              onClick={play}
             >
               <path d="M360-320h80v-320h-80v320Zm160 0h80v-320h-80v320ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
             </svg>
